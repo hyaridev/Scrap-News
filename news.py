3367053908b49bd1a82a848369869307bb5f20aa
Mohammad Hyari <hyaridev98@gmail.com>
2021-03-16 16:44:35 +0200

    adding extra fucntionality

--- news.py
+++ news.py
@@ -19,7 +19,20 @@ news_list = news_list[0:5]
 
 for news in news_list:
      notification.notify(title = "Daily News", message=news.title.text + '\nPuplished on :'+news.pubDate.text, timeout=20)
+     
 webbrowser.open(website, new=1, autoraise=True)    
+
+news_file = open('News_List.txt', 'w')
+#novel_file.write(response.text[:178978])
+for chunk in news_list:
+     news_file.write("Title: ")
+     news_file.write(news.title.text)
+     news_file.write(news.pubDate.text)
+     news_file.write("\nNews Body: ")
+     news_file.write(news.text[166:])
+     news_file.write("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+news_file.close()
 
 time.sleep(1200)
 
